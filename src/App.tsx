0109ea2c60a71482d57bf72d88b9707b215ac48d
Christopher Powroznik <chrispow96@gmail.com>
2019-02-02 01:36:48 -0800

    Database created

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,54 @@ import * as React from 'react';
 import './App.css';
 
 import logo from './logo.svg';
+import {defaultDatabase, IDatabase, IPartner, IUser, partnerName, userName} from "./data/database";
 
-class App extends React.Component {
+class App extends React.Component<IAppProps, IAppState> {
 
-    public render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+	public state: IAppState = {
+		database: defaultDatabase,
+		partnerKey: partnerName.RBC,
+		userKey: userName.MICHELLE,
+	};
+
+	constructor(props: IAppProps) {
+		super(props);
+	}
+
+	private async loginUser(username: string, password: string): Promise<boolean> {
+		const user: IUser = this.state.database.partners[this.state.partnerKey].users[username];
+		return !(user === undefined || user.password !== password);
+	}
+
+	private async loginPartner(username: string, password: string): Promise<boolean> {
+		return true;
+	}
+
+
+	public render() {
+		return (
+			<div className="App">
+
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.tsx</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
+}
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+	database: IDatabase,
+	partnerKey: partnerName,
+	userKey: userName,
 }
 
 export default App;
